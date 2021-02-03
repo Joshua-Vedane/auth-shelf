59c1c81c5cb053a40d9400bdb92344197bb10c8b
Kevin Burk <percburk@gmail.com>
2021-02-03 17:37:35 -0600

    fixed some formatting

--- src/redux/sagas/AddToShelf.saga.js
+++ src/redux/sagas/AddToShelf.saga.js
@@ -1,32 +1,29 @@
 import axios from 'axios';
-import { put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { put, takeEvery } from 'redux-saga/effects';
 
 function* addToSelf(action) {
-    console.log(action.payload);
-    
-    try {
-        const itemToAdd = {
-            description: action.payload.description,
-            image: action.payload.image_url,
-        }
+  console.log(action.payload);
 
-        console.log(itemToAdd);
-        
-        const response = yield axios.post('/api/shelf', itemToAdd);
-        console.log('Adding to shelf', itemToAdd);
-        console.log('response from add:', response.data);
+  try {
+    const itemToAdd = {
+      description: action.payload.description,
+      image: action.payload.image_url,
+    };
 
-        yield put({type: 'FETCH_SHELF'})
-        
-    } catch (err) {
-        console.log('Error posting to shelf', err);
-        
-    }
+    console.log(itemToAdd);
+
+    const response = yield axios.post('/api/shelf', itemToAdd);
+    console.log('Adding to shelf', itemToAdd);
+    console.log('response from add:', response.data);
+
+    yield put({ type: 'FETCH_SHELF' });
+  } catch (err) {
+    console.log('Error posting to shelf', err);
+  }
 }
 
 function* addToShelfSaga() {
-    yield takeEvery('ADD_ITEM', addToSelf)
+  yield takeEvery('ADD_ITEM', addToSelf);
 }
 
 export default addToShelfSaga;
-
--- src/redux/sagas/DeleteItem.saga.js
+++ src/redux/sagas/DeleteItem.saga.js
@@ -1,28 +1,26 @@
 import axios from 'axios';
-import { put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { put, takeEvery } from 'redux-saga/effects';
 
 function* deleteFromShelf(action) {
-    console.log('payload:', action.payload);
-    
-    try {
-        const itemToDelete = action.payload.item_id
+  console.log('payload:', action.payload);
 
-        console.log(itemToDelete);
-        
-        const response = yield axios.delete(`/api/shelf/${itemToDelete}`);
-        console.log('Deleting from shelf', itemToDelete);
-        console.log('response from delete:', response.data);
+  try {
+    const itemToDelete = action.payload.item_id;
 
-        yield put({type: 'FETCH_SHELF'})
-        
-    } catch (err) {
-        console.log('Error deleting from shelf', err);
-        
-    }
+    console.log(itemToDelete);
+
+    const response = yield axios.delete(`/api/shelf/${itemToDelete}`);
+    console.log('Deleting from shelf', itemToDelete);
+    console.log('response from delete:', response.data);
+
+    yield put({ type: 'FETCH_SHELF' });
+  } catch (err) {
+    console.log('Error deleting from shelf', err);
+  }
 }
 
 function* deleteFromShelfSaga() {
-    yield takeEvery('DELETE_ITEM', deleteFromShelf)
+  yield takeEvery('DELETE_ITEM', deleteFromShelf);
 }
 
 export default deleteFromShelfSaga;
